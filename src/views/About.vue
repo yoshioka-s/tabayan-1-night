f4bf3a318865ef9ad09825d96c329bbc0ccf103f
yoshioka-s <yoshioka63@gmail.com>
2019-08-09 01:14:03 +0900

    staff roll

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>企画</h2>
+    <p>田林 洋</p>
+    <h2>シナリオ</h2>
+    <p>吉岡 周</p>
+    <h2>設計</h2>
+    <p>吉岡 周</p>
+    <h2>開発</h2>
+    <p>吉岡 周</p>
+    <h2>キャラクターデザイン</h2>
+    <p>吉岡 周</p>
+    <h2>Special Thanks</h2>
+    <p>父</p>
+    <p>母</p>
+    <p>姉</p>
+    <p>妹１</p>
+    <p>妹２</p>
   </div>
 </template>
