897edd173d7c7ae743342a0ddf59acacc32f9113
yoshioka-s <yoshioka63@gmail.com>
2019-08-08 22:51:53 +0900

    stories

---
+++ src/components/BattleScreen.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+export default class BattleScreen extends Vue {
+  @Prop() private monster!: object;
+}
+</script>
+
+<style>
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,130 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <form v-if="scene.prompt==='hero.name'" @submit.prevent="next">
+      <input v-model="hero.name" />
+      <input type="submit" value="確定" />
+      <p>{{ hero.name }}</p>
+    </form>
+    <form v-if="scene.options" @submit.prevent="next">
+      <button v-for="(option, i) in scene.options" :key="i" @click="select(option)">{{ option }}</button>
+    </form>
+    <div class="text" @click="next">
+      <span v-show="scene.speaker">{{ scene.speaker }}:</span>
+      <div v-if="scene.aiduchi">{{aiduchi}}</div>
+      {{ text }}
+    </div>
+    <BattleScreen v-if="scene.monster" :monster="scene.monster"></BattleScreen>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import _ from "lodash";
+import { Hero } from "@/classes/hero";
+import BattleScreen from "./BattleScreen.vue";
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+  sceneIndex: number = 0;
+  answers: Array<string> = [];
+
+  hero: Hero = {
+    name: "ヒロシ"
+  };
+  private next() {
+    this.sceneIndex++;
+  }
+  get scene() {
+    return scenes[this.sceneIndex];
+  }
+  get text() {
+    let text: string = this.scene.text;
+    if (text.indexOf("${") === -1) {
+      return text;
+    }
+    const startIndex = text.indexOf("${");
+    const stopIndex = text.indexOf("}");
+    const propName = text.substring(startIndex + 2, stopIndex);
+    const value = _.get(this, propName);
+    return `${text.slice(0, startIndex)}${value}${text.slice(stopIndex + 1)}`;
+  }
+  select(option: string) {
+    this.answers.push(option);
+    this.next();
+  }
+  get aiduchi() {
+    const index = Math.floor(Math.random() * aiduchis.length);
+    return aiduchis[index];
+  }
 }
+
+const scenes = [
+  {
+    speaker: "母",
+    text: "いつまで寝てるんだい？早く起きなさい！今日は元服の儀式だろう？"
+  },
+  { speaker: "母", text: "ええと、お前の名前はなんだったっけ？" },
+  { prompt: "hero.name", text: "あなたの名前を入力してください。" },
+  {
+    speaker: "母",
+    text: "ああそうだったね、おまえの名前は ${hero.name} だった！"
+  },
+  {
+    text: "今どう思いましたか？",
+    options: [
+      "新しい冒険にわくわくする！",
+      "無理やり起こすなんてひどいな！",
+      "明日晴れるといいな",
+      "つまんない"
+    ]
+  },
+  { aiduchi: true, text: "" },
+  {
+    speaker: "母",
+    text: "外はまものがいっぱいでるらしいよ。気をつけていっておいで。"
+  },
+  { text: "${hero.name} は旅立った。" },
+  { monster: { name: "たばやん", hp: 10, attack: 3 }, text: "" }
+];
+const aiduchis = [
+  "ほうほう、そうきましたか。",
+  "そうなんですね。",
+  "まじっすか！",
+  "さすがっすね。",
+  "なるほどっすね。"
+];
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-h3
-  margin 40px 0 0
+h3 {
+  margin: 40px 0 0;
+}
 
-ul
-  list-style-type none
-  padding 0
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-li
-  display inline-block
-  margin 0 10px
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-a
-  color #42b983
+a {
+  color: #42b983;
+}
+
+.text {
+  display: inline-block;
+  border-color: #ffffff;
+  border-width: 3px;
+  border-style: solid;
+  border-radius: 8px;
+  height: 200px;
+  width: 600px;
+  padding: 20px;
+  text-align: left;
+}
 </style>
